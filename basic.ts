49d999858a140b6b571722fb0d38529d6328ee48
Hammad Sheikh-Bin-Nadeem <64545966+hammad-air@users.noreply.github.com>
2024-08-02 21:30:12 +0500

    new added

---
+++ basic.ts
@@ -0,0 +1,6 @@
+console.log("Hello typescript")
+
+let num:number = 6    //= assign the type 
+
+// num = "hammad"  
+// console.log(num)
--- index.ts
+++
@@ -1,6 +0,0 @@
-console.log("Hello typescript")
-
-let num:number = 6
-
-num = "hammad"
-console.log(num)
